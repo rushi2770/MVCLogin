bb0d2dd390ab9165ea091c0eafeb716c445fce57
rushi2770 <rushikeshchikka@gmail.com>
2018-04-06 12:08:23 -0700

    Add project files.

---
+++ MVCLogin/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLogin.Models;
+
+namespace MVCLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Display()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Index(MVCLogin.Models.Product productModel)
+        {
+            using (ShoppingEntities db = new ShoppingEntities())
+            {
+                int xyx = Convert.ToInt32(Session["ProductId"]);
+                var data = db.Proc_Product().ToList();
+                ViewBag.productDetails = data;
+                //Product productDetails = db.Products.Where(c => c.ProductId == xyx).FirstOrDefault();
+                if (ViewBag.productDetails == null)
+                {
+                    productModel.ErrorMessage = "No images";
+                    return RedirectToAction("Display", ViewBag.productDetails);
+                }
+                else
+                {
+                    return RedirectToAction("Display", "Home");
+                }
+            }
+                       
+        }        
+    }
+}
---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLogin.Models;
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(User userModel)
+        {
+            using (ShoppingEntities db = new ShoppingEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();                
+                if (userDetails == null)
+                {
+                    userModel.ErrorMessage = "Invalid User Name or Password";
+                    return View("Index", userModel);
+                }
+                else
+                {
+
+                    Session["Id"] = userDetails.UserId;
+                    Session["UserName"] = userDetails.UserName;
+                    Session["ProductId"] = userDetails.ProductId;
+                    return RedirectToAction("Index", "Home",userModel);
+                }
+            }
+        }
+        
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
